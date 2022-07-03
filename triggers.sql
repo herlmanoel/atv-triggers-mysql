9795996e3faf3bbb74853976bbf8fe6893d9f567
Antonio Higino B L Medeiros <antoniobisneto98@gmail.com>
2022-07-03 20:28:59 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -2,23 +2,23 @@ DELIMITER $$
 CREATE TRIGGER insert_ItensVenda AFTER INSERT ON ItensVenda
     FOR EACH ROW
         BEGIN
-            update Produtos set Estoque = Estoque new.Quantidade where Referencia = new. Produto;
+            update Produtos set Estoque = Estoque new.Quantidade where Referencia = new.Produto;
 END$$
 
 DELIMITER $$
-CREATE TRIGGER insert_ItensVenda AFTER DELETE ON ItensVenda
+CREATE TRIGGER delete_ItensVenda AFTER DELETE ON ItensVenda
     FOR EACH ROW
     BEGIN
-        update Produtos set Estoque Estoque + new.Quantidade where Referencia = new. Produto;
+        update Produtos set Estoque Estoque + new.Quantidade where Referencia = new.Produto;
 END$$
 
 DELIMITER $$
-CREATE TRIGGER insert ItensVenda AFTER UPDATE ON ItensVenda
+CREATE TRIGGER update_ItensVenda AFTER UPDATE ON ItensVenda
     FOR EACH ROW
         BEGIN
-            IF (new.Quantidade > old. Quantidade) then
-                update Produtos set Estoque = Estoque + (new. Quantidade old. Quantidade) where Referencia = new. Produto;
+            IF (new.Quantidade > old.Quantidade) then
+                update Produtos set Estoque = Estoque + (new.Quantidade old.Quantidade) where Referencia = new.Produto;
             ELSEIF new.Quantidade < old.Quantidade then
-                update Produtos set Estoque Estoque (new.Quantidade old.Quantidade) where Referencia = new. Produto;
+                update Produtos set Estoque Estoque (new.Quantidade old.Quantidade) where Referencia = new.Produto;
             END IF;
 END$$
