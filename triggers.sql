03e26f56b9748f85dab116672b918f990985a641
Antonio Higino B L Medeiros <antoniobisneto98@gmail.com>
2022-07-03 21:09:53 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -2,14 +2,14 @@ DELIMITER $$
 CREATE TRIGGER insert_ItensVenda AFTER INSERT ON ItensVenda
     FOR EACH ROW
         BEGIN
-            update Produtos set Estoque = Estoque + new.Quantidade where Referencia = new.Produto;
+            update Produtos set Estoque = Estoque - new.Quantidade where Referencia = new.Produto;
 END$$
 
 DELIMITER $$
 CREATE TRIGGER delete_ItensVenda AFTER DELETE ON ItensVenda
     FOR EACH ROW
         BEGIN
-            update Produtos set Estoque = Estoque - old.Quantidade where Referencia = old.Produto;
+            update Produtos set Estoque = Estoque + old.Quantidade where Referencia = old.Produto;
 END$$
 
 DELIMITER $$
